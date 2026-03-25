187110eb4ef4fc99e118915062e5deb4c7bb2a47
aimeritething <aimeritething@gmail.com>
2026-03-25 17:50:45 +0800

    feat(dataflow): wire AuthProvider into app entry point

--- dataflow/src/main.tsx
+++ dataflow/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
 import { graphqlClient } from '@/src/config/graphql-client';
+import { AuthProvider } from '@/src/contexts/AuthContext';
 import { ConnectionProvider } from '@/contexts/ConnectionContext';
 import { MainLayout } from '@/components/layout/MainLayout';
 import './globals.css';
@@ -9,9 +10,11 @@ import './globals.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={graphqlClient}>
-      <ConnectionProvider>
-        <MainLayout />
-      </ConnectionProvider>
+      <AuthProvider>
+        <ConnectionProvider>
+          <MainLayout />
+        </ConnectionProvider>
+      </AuthProvider>
     </ApolloProvider>
   </React.StrictMode>
 );
